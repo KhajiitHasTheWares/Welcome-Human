6b82f1b73e0df5f253d8769daf0a852ee473052b
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-06-25 09:39:36 -0400

    Update and rename .js to Welcome-Human.js

---
+++ Welcome-Human.js
@@ -0,0 +1,18 @@
+const expr = "welcomeHuman";
+switch (expr) {
+    default: //the generic greeting
+    console.log("Can I help you?");
+    break;
+  case "Family": //the Family greeting
+    console.log("Welcome to the shit show");
+    break;
+  case "Dad": //the Dad greeting
+    console.log("*hugs*");
+    break;
+  case "Medical Worker": //the non emergency Medical greeting
+    console.log("I've been expecting you!");
+    break;
+  case "Maintenance Worker": //the Maintenance greeting
+    console.log("Let me show you the issue.");
+    break;
+}
