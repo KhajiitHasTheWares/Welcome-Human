982694620f2ea91f926fa2d05a9f71cb596a0a94
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2026-04-11 16:33:43 -0400

    Add C switch statement for welcoming different Visitors

---
+++ Welcome-Human.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int welcomeHuman = "Visitor";
+
+switch (welcomeHuman) {
+case "Dad":
+printf("*Hug*");
+break;
+case "CST":
+printf("Been expecting you");
+break;
+case "Maintenance":
+printf("Here's the issue");
+break;
+case "Friends":
+printf("Wassup");
+break;
+}
